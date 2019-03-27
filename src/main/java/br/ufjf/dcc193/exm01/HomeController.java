5205a18da8d36a25be0399651c8ee8f04f574631
ramonlarivoir <rlarivoir@hotmail.com>
2019-03-27 19:59:40 -0300

    Adiciona controlador inicial

---
+++ src/main/java/br/ufjf/dcc193/exm01/HomeController.java
@@ -0,0 +1,17 @@
+package br.ufjf.dcc193.exm01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * HomeController
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("index.html")
+    String home(){
+        return "/WEB-INF/home.jsp";
+    }
+    
+}
