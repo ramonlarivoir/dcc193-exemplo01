2098c6bf47a15539275b93e6fcbcf6a04b648865
ramonlarivoir <rlarivoir@hotmail.com>
2019-04-03 19:44:45 -0300

    Adiciona CRUD

---
+++ src/main/java/br/ufjf/dcc193/exm01/PessoaRepository.java
@@ -0,0 +1,11 @@
+package br.ufjf.dcc193.exm01;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * PessoaRepository
+ */
+public interface PessoaRepository extends JpaRepository<Pessoa,Long>{
+
+    
+}
